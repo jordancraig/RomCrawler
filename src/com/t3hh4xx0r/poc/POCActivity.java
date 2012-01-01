899ff9fccae851edc3341528516cf3cabd814103
r2DoesInc <r2doesinc@gmail.com>
2012-01-01 05:28:49 -0600

    Return urls of all .zip files from input web page

--- src/com/t3hh4xx0r/poc/POCActivity.java
+++ src/com/t3hh4xx0r/poc/POCActivity.java
@@ -1,5 +1,6 @@
 package com.t3hh4xx0r.poc;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
@@ -17,29 +18,39 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import android.app.Activity;
+import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class POCActivity extends Activity {
+public class POCActivity extends ListActivity  {
 	TextView mURLText;
 	TextView mZipText;
 	String mLinkURL;
 	String mURLURL;
 	static String finalName;
+    private ArrayAdapter<String> linkArrayAdapter;
+    Elements links;
+    String strClean;
+    Document doc;
+
 	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        mURLText = (TextView) findViewById(R.id.url);
-        mZipText = (TextView) findViewById(R.id.zip);
-        
+
+        final ArrayList<String> linkArray = new ArrayList<String>();
+        linkArrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item,
+            R.id.itemName, linkArray);
+        setListAdapter(linkArrayAdapter);
+
         try {
 		getNames();
         } catch (Exception e) {
@@ -47,8 +58,8 @@ public class POCActivity extends Activity {
         }
 
     }
-    
-    public void getNames() throws IOException {
+
+	public void getNames() throws IOException {
 	    new Thread(new Runnable() {
 	    	public void run() {
 	    		try {
@@ -67,70 +78,74 @@ public class POCActivity extends Activity {
 	    				buf.append((char) ch);
 	    			}
 	    			String strMessy = buf.toString();
-	    			String strClean = strMessy.replaceAll("\\<.*?>","");
-
-	    			getFinalName(strClean);
-    		
+	    			strClean = strMessy.replaceAll("\\<.*?>","");
+		        	POCActivity.this.runOnUiThread(new Runnable() {
+	        		@Override
+	        			public void run() {
+	        				getFinalName(strClean);
+	        			}
+		        	});			    			
 	    		} catch (Exception e) {
 	    			e.printStackTrace();
 	    		}
-	    	mHandler.sendEmptyMessage(1);
 	    	}
 	    }).start();
     }
     	
-    public void getLinks() throws IOException {
+	public void getLinks() throws IOException {
 		new Thread(new Runnable() {
-            public void run() {
-            	mURLURL = "http://rootzwiki.com/topic/12083-unsecured-stock-bootimg402/";
-            	Document doc = null;
-				try {
-					doc = Jsoup.connect(mURLURL).get();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-		        Elements links = doc.select("a[href]");
-
-            	for (Element link : links) {
-            		if (link.attr("abs:href").contains(finalName)){
-            			mLinkURL = finalName + " " + " " + link.attr("abs:href");
-            		}
-            		//String strClean = zip.absUrl("href"); //foo bar blah.zip	
-            		//getFinalName(strClean);
-            		//String[] linkUrlsFull = linkUrlFull.split(".zip");
-            		//String linkUrl = linkUrlsFull[0]; //ends string before .zip -- foo bar blah
-            		//String[] parts = linkUrl.split(" ");
-            		//String lastWord = parts[parts.length - 1]; //cuts all but last word -- blah
-            		//mLinkURL = lastWord + ".zip"; //adds back .zip extension -- blah.zip
-            		Log.d("POC", "\"" + mLinkURL + "\"");
-            	} mHandler.sendEmptyMessage(0);
+	           public void run() {
+	            	mURLURL = "http://rootzwiki.com/topic/12083-unsecured-stock-bootimg402/";
+					try {
+						doc = Jsoup.connect(mURLURL).get();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		        	POCActivity.this.runOnUiThread(new Runnable() {
+		        		@Override
+		        		public void run() {
+		        			Elements links = doc.select("a[href]");
+		        			for (int i=0; i<linkArrayAdapter.getCount(); i++) {
+		        				finalName = linkArrayAdapter.getItem(i);
+		        				for (Element link : links) {
+		        					if (link.attr("abs:href").contains(finalName)){
+		        						linkArrayAdapter.remove(finalName);
+		        						linkArrayAdapter.add(link.attr("abs:href"));
+		        					}
+		        				}
+		        			}
+			        	}
+				    });
             } 
 		}).start();
     }
     
     public void getFinalName(String strClean) {
+        DBAdapter db = new DBAdapter(this); 
+
     	//should loop through until string no longer contains any .zip files.
-    	//while (strClean.contains(".zip")){
+    	while (strClean.contains(".zip")){
     		String trimmed = strClean.substring(0, strClean.lastIndexOf(".zip"));
     		String[] parts = trimmed.split("\n");
     		String lastWord = parts[parts.length - 1] + ".zip";
     		finalName = lastWord;
-    		//Log.d("POC", finalName);
-    		//strClean = new String(strClean.replace(finalName, ""));
-    	//}
+    		strClean = new String(strClean.replace(finalName, ""));
+			linkArrayAdapter.add(finalName);
+    	}
+    	mHandler.sendEmptyMessage(1);
     }
     
 	final Handler mHandler = new Handler(){ 
         public void handleMessage (Message  msg) {
         	switch (msg.what) {
         	case 0:
-        		mURLText.setText(mURLURL);
-        		mZipText.setText(mLinkURL);
+        		//mURLText.setText(mURLURL);
+        		//mZipText.setText(mLinkURL);
         		break;
         	case 1:
         		try {
+        			Log.d("POC", "HANDLER MESSAGE RECEIVED!");
         			getLinks();
         		} catch (IOException e) {
         			// TODO Auto-generated catch block
