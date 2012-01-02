24e93d3f023bcfa55911f12a5249186c366f614d
r2DoesInc <r2doesinc@gmail.com>
2012-01-02 02:36:55 -0600

    Scrape zip URLS even if link isnt named foo.zip Hyperlinks for example
    Ensure that links are only added once
    Only update listView once all links are parsed

--- src/com/t3hh4xx0r/poc/POCActivity.java
+++ src/com/t3hh4xx0r/poc/POCActivity.java
@@ -1,14 +1,13 @@
 package com.t3hh4xx0r.poc;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,25 +16,24 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-import android.app.Activity;
 import android.app.ListActivity;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 
 import android.util.Log;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
 
 public class POCActivity extends ListActivity  {
 	TextView mURLText;
 	TextView mZipText;
 	String mLinkURL;
-	String mURLURL;
+	String mURLURL ="http://rootzwiki.com/topic/12083-unsecured-stock-bootimg402/page__fromsearch__1";
+	//String mURLURL = "http://rootzwiki.com/topic/12451-rom-android-open-kang-project-toro-build-2012-dec-31/";
 	static String finalName;
     private ArrayAdapter<String> linkArrayAdapter;
+    ArrayList<String> linkArray;
     Elements links;
     String strClean;
     Document doc;
@@ -46,13 +44,12 @@ public class POCActivity extends ListActivity  {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        final ArrayList<String> linkArray = new ArrayList<String>();
+        linkArray = new ArrayList<String>();
         linkArrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item,
             R.id.itemName, linkArray);
-        setListAdapter(linkArrayAdapter);
-
+        
         try {
-		getNames();
+        getNames();
         } catch (Exception e) {
         	e.printStackTrace();
         }
@@ -63,7 +60,7 @@ public class POCActivity extends ListActivity  {
 	    new Thread(new Runnable() {
 	    	public void run() {
 	    		try {
-	    			URL url = new URL("http://rootzwiki.com/topic/12083-unsecured-stock-bootimg402/");
+	    			URL url = new URL(mURLURL);
 	    			URLConnection con = url.openConnection();
 	    			Pattern p = Pattern.compile("text/html;\\s+charset=([^\\s]+)\\s*");
 	    			Matcher m = p.matcher(con.getContentType());
@@ -106,24 +103,38 @@ public class POCActivity extends ListActivity  {
 		        		@Override
 		        		public void run() {
 		        			Elements links = doc.select("a[href]");
-		        			for (int i=0; i<linkArrayAdapter.getCount(); i++) {
-		        				finalName = linkArrayAdapter.getItem(i);
 		        				for (Element link : links) {
-		        					if (link.attr("abs:href").contains(finalName)){
-		        						linkArrayAdapter.remove(finalName);
-		        						linkArrayAdapter.add(link.attr("abs:href"));
-		        					}
-		        				}
-		        			}
+				        			for (int i=0; i<linkArrayAdapter.getCount(); i++) {
+				        				finalName = linkArrayAdapter.getItem(i);
+				       					if (link.attr("abs:href").contains(finalName)) {
+				       						linkArrayAdapter.remove(finalName);
+				       						linkArrayAdapter.add(link.attr("abs:href"));
+				       					}
+				       				}
+		        					if (link.attr("abs:href").contains(".zip")) {	
+		       							linkArrayAdapter.add(link.attr("abs:href"));
+		       						}
+		       					}
+		        			setList();
 			        	}
 				    });
             } 
 		}).start();
     }
     
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public void setList() {
+		HashSet hashSet = new HashSet(linkArray);
+ 	    linkArray = new ArrayList(hashSet);
+		for (Iterator c = linkArray.iterator(); c.hasNext();) {
+			Log.d("POC", (String)c.next());
+		}
+	    linkArrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item,
+             R.id.itemName, linkArray);
+        setListAdapter(linkArrayAdapter);
+	}
+	
     public void getFinalName(String strClean) {
-        DBAdapter db = new DBAdapter(this); 
-
     	//should loop through until string no longer contains any .zip files.
     	while (strClean.contains(".zip")){
     		String trimmed = strClean.substring(0, strClean.lastIndexOf(".zip"));
@@ -133,19 +144,14 @@ public class POCActivity extends ListActivity  {
     		strClean = new String(strClean.replace(finalName, ""));
 			linkArrayAdapter.add(finalName);
     	}
-    	mHandler.sendEmptyMessage(1);
+    	mHandler.sendEmptyMessage(0);
     }
     
 	final Handler mHandler = new Handler(){ 
         public void handleMessage (Message  msg) {
         	switch (msg.what) {
         	case 0:
-        		//mURLText.setText(mURLURL);
-        		//mZipText.setText(mLinkURL);
-        		break;
-        	case 1:
-        		try {
-        			Log.d("POC", "HANDLER MESSAGE RECEIVED!");
+           		try {
         			getLinks();
         		} catch (IOException e) {
         			// TODO Auto-generated catch block
